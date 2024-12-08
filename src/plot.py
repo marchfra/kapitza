2f9502d2e459a043b780988459ad1b1b4ced7c00
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2024-12-08 12:53:40 +0100

    Code cleanup

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,3 @@
-import random
-
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -8,97 +6,100 @@ from my_formatter import multiple_formatter
 
 
 def check_stability(a: float, sigma: float) -> bool:
+    """Check if the Kapitza pendulum is stable according to the Multiscale Method."""
     return a**2 > 2 * sigma
 
 
 plt.style.use(["grid", "science", "notebook", "mylegend"])
 
-df = pd.read_csv("data/stability.csv")
 
-TOL = 1e-2
+def preprocess_stability(tol: float = 1e-2) -> pd.DataFrame:
+    """Preprocess the stability data."""
 
-df["stability"] = df["endpoint"].apply(lambda x: x < TOL)
-df["stability"] = df["stability"].astype(bool)
-df["is_stable"] = check_stability(df["a"], df["sigma"])
+    df = pd.read_csv("data/stability.csv")
 
-stable_index = df["stability"]
-stable = df[stable_index]
-unstable = df[~stable_index]
+    df["stability"] = df["endpoint"].apply(lambda x: x < tol)
+    df["stability"] = df["stability"].astype(bool)
+    df["is_stable"] = check_stability(df["a"], df["sigma"])
 
-fig, ax = plt.subplots(1, 1)
-MARKER = "s"
-SKIP = 1
-# ax.scatter(
-#     df["a"][::SKIP],
-#     df["sigma"][::SKIP],
-#     color=check_stability(df["a"][::SKIP], df["sigma"][::SKIP]).map(
-#         {True: "green", False: "red"}
-#     ),
-#     marker="o",
-# )
-ax.scatter(
-    stable["a"][::SKIP],
-    stable["sigma"][::SKIP],
-    color="green",
-    label="Stable",
-    marker=MARKER,
-)
-ax.scatter(
-    unstable["a"][::SKIP],
-    unstable["sigma"][::SKIP],
-    color="red",
-    label="Unstable",
-    marker=MARKER,
-)
+    return df
 
-# stable_index = df["is_stable"]
-# stable = df[stable_index]
-# unstable = df[~stable_index]
-# MARKER = "."
-# ax.scatter(
-#     stable["a"][::SKIP],
-#     stable["sigma"][::SKIP],
-#     color="green",
-#     label="Stable",
-#     marker=MARKER,
-# )
-# ax.scatter(
-#     unstable["a"][::SKIP],
-#     unstable["sigma"][::SKIP],
-#     color="red",
-#     label="Unstable",
-#     marker=MARKER,
-# )
 
-a_max = np.sqrt(2 * df["sigma"].max())
-a = np.linspace(df["a"].min(), a_max)
-ax.plot(a, 0.5 * a**2)
+def plot_stability(skip: int = 1) -> None:
+    """Plots the stability of the Kapitza pendulum on the a-sigma plane."""
 
-ax.set_title("Stability of the Kapitza pendulum")
-ax.set_xlabel(r"$a$")
-ax.set_ylabel(r"$\sigma$")
+    df = preprocess_stability()[::skip]
 
-ax.legend()
+    stable_index = df["stability"]
+    stable = df[stable_index]
+    unstable = df[~stable_index]
 
-fig.tight_layout()
+    fig, ax = plt.subplots(1, 1)
+    marker = "s"
+    # ax.scatter(
+    #     df["a"],
+    #     df["sigma"],
+    #     color=df["is_stable"].map({True: "green", False: "red"}),
+    #     marker="o",
+    # )
+    ax.scatter(
+        stable["a"], stable["sigma"], color="green", label="Stable", marker=marker
+    )
+    ax.scatter(
+        unstable["a"], unstable["sigma"], color="red", label="Unstable", marker=marker
+    )
 
-df = pd.read_csv("data/trajectory.csv")
+    # Plot multiscale method separation line
+    a_max = np.sqrt(2 * df["sigma"].max())
+    a = np.linspace(df["a"].min(), a_max)
+    ax.plot(a, 0.5 * a**2)
 
-fig, ax = plt.subplots(1, 1)
-for a in df["a"].unique()[:: SKIP**2]:
-    for sigma in df["sigma"].unique()[:: SKIP**2]:
-        data = df[df["a"] == a]
-        trajectory = data[data["sigma"] == sigma]
-        ax.plot(
-            trajectory["t"], trajectory["theta"], label=f"a = {a:.2g}, σ = {sigma:.2g}"
-        )
+    ax.set_title("Stability of the Kapitza pendulum")
+    ax.set_xlabel(r"$a$")
+    ax.set_ylabel(r"$\sigma$")
 
-ax.set_title("Trajectory of the Kapitza pendulum")
-ax.set_xlabel(r"$t$")
-ax.set_ylabel(r"$\theta(t)$")
+    ax.legend()
 
-ax.yaxis.set_major_locator(plt.MultipleLocator(np.pi / 6))
-ax.yaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter(6)))
+    fig.tight_layout()
 
-fig.tight_layout()
-plt.show()
+
+def plot_trajectories(skip: int = 2) -> None:
+    """Plots the trajectories of the Kapitza pendulum for various values of a-sigma."""
+
+    df = pd.read_csv("data/trajectory.csv")
+
+    fig, ax = plt.subplots(1, 1)
+    for a in df["a"].unique()[::skip]:
+        for sigma in df["sigma"].unique()[::skip]:
+            filt = (df["a"] == a) & (df["sigma"] == sigma)
+            trajectory = df[filt]
+            ax.plot(
+                trajectory["t"],
+                trajectory["theta"],
+                label=r"$a$ = " + f"{a:.2g}, " + r"$\sigma$ = " + f"{sigma:.2g}",
+            )
+
+    ax.set_title("Trajectory of the Kapitza pendulum")
+    ax.set_xlabel(r"$t$")
+    ax.set_ylabel(r"$\theta(t)$")
+
+    ax.legend(ncol=3)
+
+    ax.yaxis.set_major_locator(plt.MultipleLocator(np.pi / 6))
+    ax.yaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter(6)))
+
+    fig.tight_layout()
+
+
+def main() -> None:
+    """Main function."""
+
+    plot_stability()
+
+    plot_trajectories(20)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
